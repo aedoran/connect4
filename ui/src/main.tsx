ebdfa85727075e4906146de9aadaf2593e957aa9
aedoran <aedoran@gmail.com>
2025-06-25 19:15:02 -0700

    feat(ui): add Apollo client via env

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphqlClient';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
